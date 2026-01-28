ebf2d89533aa7fd8eb35ada79a3bbc786784c27f
Vanessa Perez <vaneppv@gmail.com>
2026-01-27 22:37:27 -0400

    feat: crear mi repositorio personal de Programación II

---
+++ Clase1/Punteros.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <conio.h>
+
+struct persona
+{
+    char nombre[30];
+    int edad;
+}
+persona1{"Vanessa", 28},
+persona2{"Edwar", 11};
+
+
+
+int main(){
+
+    
+    
+    
+
+
+    getch();
+    return 0;
+}
+
+
